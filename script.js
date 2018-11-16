1bbf17b2a00c3efbafb2bace6f67f263447d2573
Pavel <pavel0408@gmail.com>
2018-11-16 17:05:41 +0300

    Функция для получения массива простых чисел через решето эратосфена

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-var num1 = +prompt("Введите число", "");
-var fibBinet = function (n) {
-  var f = (1 + Math.sqrt(5)) / 2;
-  var fib = (Math.pow(f,n))/Math.sqrt(5);
-  return Math.round(fib);
+var max = +prompt('введите максимальное значение', '');
+var arrPrime = function (maxNumber) {
+    var start = [];
+    for (var i = 0; i < maxNumber; i++) {
+        start.push(i);
+    }
+    console.log(start);
+    for (var k = 2; k <= maxNumber; k++) {
+        for (var j = k; j <= maxNumber; j = j + k) {
+            if (j != k) {
+                delete start[j];
+            }
+        }
+    }    
+    var primeNumbers = [];
+    var summ = 0;
+    for (i = 2; i < maxNumber; i++) {
+        if (start[i]) {
+            primeNumbers.push(start[i]);
+            summ+=start[i];
+        }
+    }
+    
+    return primeNumbers;
 }
-alert(fibBinet(num1));
+
+alert(arrPrime(max));
