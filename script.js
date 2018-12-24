0b249bc17427e28663398acbdcbd40d22dd3e58a
Pavel <pavel0408@gmail.com>
2018-12-24 16:43:03 +0300

    Удаляет прерданный класс

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-var max = +prompt('введите максимальное значение', '');
-var arrPrime = function (maxNumber) {
-    var start = [];
-    for (var i = 0; i < maxNumber; i++) {
-        start.push(i);
-    }
-    console.log(start);
-    for (var k = 2; k <= maxNumber; k++) {
-        for (var j = k; j <= maxNumber; j = j + k) {
-            if (j != k) {
-                delete start[j];
-            }
-        }
-    }    
-    var primeNumbers = [];
-    var summ = 0;
-    for (i = 2; i < maxNumber; i++) {
-        if (start[i]) {
-            primeNumbers.push(start[i]);
-            summ+=start[i];
-        }
-    }
-    
-    return primeNumbers;
+export function removeClass(obj, cls) {
+  let ownCls = obj.className;
+  if (!ownCls) {
+    return;
+  }
+  ownCls = ownCls.split(` `);
+  let counter;
+  do {
+    counter = 0;
+    ownCls.forEach((element, index) => {
+      if (element === cls) {
+        ownCls.splice(index, 1);
+        counter++;
+      }
+
+
+    });
+  } while (counter !== 0);
+  obj.className = ownCls.join(` `);
+  obj.className = obj.className.trim();
 }
 
-alert(arrPrime(max));
