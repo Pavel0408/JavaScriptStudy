9118a94187ee307c490f0d247754bebf114565d4
Pavel <pavel0408@gmail.com>
2019-03-18 14:09:18 +0300

    функция sum

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
 'use strict';
+// Напишите функцию sum, которая будет работать так:
 
-// Создайте функцию filter(arr, func), которая получает массив arr и возвращает новый, в который входят только те элементы arr, для которых func возвращает true.
-// Создайте набор «готовых фильтров»: inBetween(a,b) – «между a,b», inArray([...]) – "в массиве [...]". Использование должно быть таким:
-// filter(arr, inBetween(3,6)) – выберет только числа от 3 до 6,
-// filter(arr, inArray([1,2,3])) – выберет только элементы, совпадающие с одним из значений массива.
+// sum(1)(2) == 3; // 1 + 2
+// sum(1)(2)(3) == 6; // 1 + 2 + 3
+// sum(5)(-1)(2) == 6
+// sum(6)(-1)(-2)(-3) == 0
+// sum(0)(1)(2)(3)(4)(5) == 15
+// Количество скобок может быть любым.
 
-function filter(arr, func) {
-  const newArr = [];
-  arr.forEach((elem) => {
-    if (func(elem)) {
-      newArr.push(elem);
-    }
-  });
-  return newArr;
+function sum(number) {
+  sum.akk += number;
+  return sum;
 }
 
-function inBetween(a, b) {
-  return function (elem) {
-    return ((elem >= a) && (elem <= b));
-  };
-}
+sum.akk = 0;
 
-function inArray(arr) {
-  return function (elem) {
-    if (arr.indexOf(elem) === -1) {
-      return false;
-    }
-    return true;
-  };
-}
+sum.toString = () => {
+  return sum.akk;
+};
+
